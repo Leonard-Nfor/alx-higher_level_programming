631e27f89c533b3d7a5548526b72f2ac4ebbc07b
Leonard-Nfor <leonardnforkort@gmail.com>
2023-08-11 16:57:14 -0700

    m

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__"
-    """prints all the names defined by the compiled module
+"""prints all the names defined by the compiled module
     hidden_4.pyc (please download it locally)."""
-    import hidden_4
+import hidden_4
 
-    name = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__"
-            print(name)
+name = dir(hidden_4)
+for name in names:
+    if name[:2] != "__"
+    print(name)
