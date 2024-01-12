51be63065ae2d5279c3834dfd761990ce76305ec
leonard-Nfor <leonardnforkort@gmail.com>
2024-01-12 05:51:04 -0800

    message

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+         return None
+    max_key = None
+    max_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if isinstance(value, int) and value > max_value:
+            max_key = key
+            max_value = value
+    return max_key
