30e8543020c285f5a1fcb23e5556d4adbdacc043
leonard-Nfor <leonardnforkort@gmail.com>
2024-01-23 12:28:46 -0800

    m

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification
+    You are not allowed to import any module"""
+    def __init__(self, size):
+        """initializing the size"""
+        self.__size = size
