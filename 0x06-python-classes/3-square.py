700fec8e204acbc0d830f87f328a008f8c1dca8a
leonard-Nfor <leonardnforkort@gmail.com>
2024-01-23 12:58:05 -0800

    m

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Square:
+    """define a square with optional artribute to size"""
+    def __init__(self, size=0):
+        """initializer and condition to check integer of the size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """return the area of a square"""
+            return self.__size * self.__size
