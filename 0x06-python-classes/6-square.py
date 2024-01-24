2a5acce032eeffb6ed6f38ad49108c88e6cc2ca1
leonard-Nfor <leonardnforkort@gmail.com>
2024-01-24 03:02:44 -0800

    mas

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,8 +48,12 @@ class Square:
 
     def my_print(self):
         """print to stdout"""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
