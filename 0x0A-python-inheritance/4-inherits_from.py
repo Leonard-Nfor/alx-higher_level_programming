526bf4d4fe5303615408e5d372fa7be4fb0e372c
leonard-Nfor <leonardnforkort@gmail.com>
2024-02-06 06:11:50 -0800

    m

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Direct the interpreter"""
 
+
 def inherits_from(obj, a_class):
     """check if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class
     type(obj) != a_class check if obj is not thesame as a_class
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-       return True
+        return True
     else:
         return False
